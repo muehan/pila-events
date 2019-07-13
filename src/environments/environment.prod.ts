0d8212dc316c2136f02de6cb89a23e234f2c15d0
andre <tdc@gmx.ch>
2019-07-13 22:20:49 +0200

    firebase library changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyChwmTTCGm98wluEjpnjlLcXAGdrqLbVjU",
-    authDomain: "pila-events.firebaseapp.com",
-    databaseURL: "https://pila-events.firebaseio.com",
-    projectId: "pila-events",
-    storageBucket: "pila-events.appspot.com",
-    messagingSenderId: "149768498993",
-    appId: "1:149768498993:web:100bad20cf8b3c66"
-  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyChwmTTCGm98wluEjpnjlLcXAGdrqLbVjU",
+    authDomain: "pila-events.firebaseapp.com",
+    databaseURL: "https://pila-events.firebaseio.com",
+    projectId: "pila-events",
+    storageBucket: "pila-events.appspot.com",
+    messagingSenderId: "149768498993",
+    appId: "1:149768498993:web:100bad20cf8b3c66"
+  }
 };
 
 /*
